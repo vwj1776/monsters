28f2ed1a24aaf547a0d58721d573fcf31aec5ed2
vwj1776 <vwj1776@gmail.com>
2024-05-20 14:57:59 -0600

    ExpressSettingUpNewExpressApps, added routes, appsTs and delete, post, get, getall to dragons and monsters

--- apps/expressApps/apps.ts
+++ apps/expressApps/apps.ts
@@ -2,17 +2,20 @@ import express from 'express';
 import cors from 'cors';
 import dragonsController from "./routes/dragonsController";
 import monstersController from "./routes/monstersController";
-
+import helmet from 'helmet';
 const app = express()
 
 
-const port = 3101;
+const port = 3000; // why 3000??
 const host = process.env.SERVER_URL || 'localhost';
 
 
 
 app.use(cors());
+app.use(express.json());
+app.use(helmet());
 
+app.use(express.urlencoded({extended: true} ));
 app.use('/dragons', dragonsController);
 app.use('/monsters', monstersController);
 
--- apps/expressApps/routes/monstersController.ts
+++ apps/expressApps/routes/monstersController.ts
@@ -1,7 +1,46 @@
-import express from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 import bodyParser from 'body-parser';
-
+// @ts-ignore
+import {monsterType} from "monsters/src/monsterType";
 const monstersController = express();
 monstersController.use(bodyParser.json());
+
+monstersController.get('/api/:monsterID',(req: Request, res: Response, next: NextFunction) => {
+    let monster; // const monster: monsterType = res??
+    console.log(req.params);
+    res.send(monster)
+});
+
+
+//get all
+monstersController.get('/api/monsters',(req: Request, res: Response, next: NextFunction) => {
+    console.log(req.params);
+    const monsters: any[] = []; // const monsters: monsterType = []; res??
+    res.send(monsters);
+});
+
+monstersController.post('/api/post/:monster',(req: Request, res: Response, next: NextFunction) => {
+    console.log(req.params);
+    return res.sendStatus(200);
+
+});
+
+monstersController.delete('/api/delete/:monster',(req: Request, res: Response, next: NextFunction) => {
+    console.log(req.params);
+    return res.sendStatus(200);
+});
+
+async function throwsError(){
+    throw new Error('Boy');
+}
+
+monstersController.get('/error', async (req: Request, res: Response) => {
+    try{
+        await throwsError();
+        res.sendStatus(200);
+    } catch(e) {
+        res.status(400).send('Ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh monsters');
+    }
+})
 
 export default monstersController;
