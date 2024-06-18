eda758f571be36c028f59ab3713e27d90fd25b0e
vwj1776 <vwj1776@gmail.com>
2024-06-18 15:43:42 -0600

    full stack adding modal forms and post in the controller

--- apps/Dragons/src/WelcomeToDragons.tsx
+++ apps/Dragons/src/WelcomeToDragons.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 import './index.css'
 import SubNavigation from "@churchofjesuschrist/eden-sub-navigation";
 import WorkforceFooter from "@churchofjesuschrist/eden-workforce-footer";
 import Card from "@churchofjesuschrist/eden-card";
 import { DragonType } from "./dragonType.tsx";
-import { Primary } from "@churchofjesuschrist/eden-buttons";
-import { useLocation, useNavigate } from 'react-router-dom';
-
-
+import { Primary, Secondary } from "@churchofjesuschrist/eden-buttons";
+import { useNavigate } from 'react-router-dom';
+import ToolModal from "@churchofjesuschrist/eden-tool-modal";
+import { Form, FormField, Input } from "@churchofjesuschrist/eden-form-parts";
+import Row from "@churchofjesuschrist/eden-row";
+import { Stack } from "@churchofjesuschrist/eden-tile-parts";
+import axios from "axios";
 function WelcomeToDragons() {
 
     const navigate = useNavigate();
@@ -17,71 +20,150 @@ function WelcomeToDragons() {
         navigate(path);
     }
 
+    const [open, setOpen] = useState(false); // Initialize state for the modal
+    const refReference = useRef(null); // Initialize ref for the form
+    const [error, setError] = useState<string | null>(null);
 
-  const dragon0: DragonType = {
-      name: 'Puff',
-      type: 'Nice :)',
-      powerLevel: 3,
-      image: 'https://dk.pinterest.com/pin/370632244304904603/'
-  }
-  return (
-      <>
-          <div>
-              <SubNavigation
-                  items={[
-                      {
-                          items: [
-                              {
-                                  current: '[Circular]',
-                                  onClick: () => handleNavigate('/Dragons'),
-                                  text: 'WelcomeToDragons'
-                              },
-                              {
-                                  onClick: () => handleNavigate('/Dragons/allDragons'),
-                                  text: 'DragonsDetailPage'
-                              }
-                          ],
-                          text: 'Dragons'
-                      },
-                      {
-                          items: [
-                              {
-                                  current: '[Circular]',
-                                  onClick: () => handleNavigate('/Monsters'),
-                                  text: 'WelcomeToMonsters'
-                              },
-                              {
-                                  onClick: () => handleNavigate('/Monsters/allMonsters'),
-                                  text: 'MonstersDetailPage'
-                              }
-                          ],
-                          text: 'Monsters'
-                      }
-                  ]}
-                  title={{
-                      onClick: () => handleNavigate('/'),
-                      text: 'Home App'
-                  }}
-              />
-              <div className="min-h-screen flex flex-col bg-amber-400">
-                  <p className="text-3xl font-bold underline">Welcome To Dragons</p>
-
-                  <Card depth="raised">
-                      <h1>Dragon Details</h1>
-                      <p>Name: {dragon0.name}</p>
-                      <p>Type: {dragon0.type}</p>
-                      <p>Power Level: {dragon0.powerLevel}</p>
-                      <img src={dragon0.image} />
-                  </Card>
+    const openModal = () => setOpen(true); // Function to open the modal
+    const closeModal = () => {
+        if (refReference.current) refReference.current.reset(); // Reset the form
+        setOpen(false); // Close the modal
+    };
 
 
-              </div>
 
-              <WorkforceFooter />
-          </div>
 
-      </>
-  )
+    function handleAddDragon(e: any) {
+        e.preventDefault();
+
+
+        const formData = new FormData(e.target);
+        const dragon: DragonType = {
+            name: formData.get('name') as string,
+            type: formData.get('type') as string,
+            powerLevel: formData.get('powerLevel') as number,
+            image: formData.get('image') as string
+        };
+        // if (eventId) {
+        //     data.eventId = eventId;
+        // }
+
+        upsertDragon(dragon);
+        closeModal();
+    }
+
+    async function upsertDragon(dragon: DragonType) {
+        try {
+            await axios({
+                method: 'post',
+                url: '/dragons/api/post',
+                baseURL: 'http://localhost:3000',
+                data: dragon
+            }).catch((e) => console.log(e))
+
+
+
+        } catch (err) {
+            setError('Error adding dragons you dumb dumb');
+
+        }
+    }
+
+
+
+
+
+
+    return (
+        <>
+            <div>
+                <SubNavigation
+                    items={[
+                        {
+                            items: [
+                                {
+                                    current: '[Circular]',
+                                    onClick: () => handleNavigate('/Dragons'),
+                                    text: 'WelcomeToDragons'
+                                },
+                                {
+                                    onClick: () => handleNavigate('/Dragons/allDragons'),
+                                    text: 'DragonsDetailPage'
+                                }
+                            ],
+                            text: 'Dragons'
+                        },
+                        {
+                            items: [
+                                {
+                                    current: '[Circular]',
+                                    onClick: () => handleNavigate('/Monsters'),
+                                    text: 'WelcomeToMonsters'
+                                },
+                                {
+                                    onClick: () => handleNavigate('/Monsters/allMonsters'),
+                                    text: 'MonstersDetailPage'
+                                }
+                            ],
+                            text: 'Monsters'
+                        }
+                    ]}
+                    title={{
+                        onClick: () => handleNavigate('/'),
+                        text: 'Home App'
+                    }}
+                />
+                <div className="min-h-screen flex flex-col bg-amber-400">
+                    <p className="text-3xl font-bold underline">Welcome To Dragons</p>
+                </div>
+                <WorkforceFooter />
+            </div>
+
+            {/**/}
+            <Primary onClick={openModal}>
+                Click to Add Dragon
+            </Primary>
+            <ToolModal open={open}
+                footer={<Row><Primary form=":r0:" type="submit">Submit</Primary><Secondary onClick={function(){refReference.current.reset(),setOpen(!1)}}>Cancel</Secondary></Row>}
+                header="New Dragon"
+                onClose={closeModal}
+            >
+                <Form
+                    ref={refReference}
+                    id=":r0:"
+                    method="dialog"
+                    onSubmit={(e: any) => handleAddDragon(e)}
+                >
+                    <Stack>
+                        <FormField label="Name">
+                            <Input
+                                autofocus="true"
+                                name="name"
+                                required
+                            />
+                        </FormField>
+                        <FormField label="Type">
+                            <Input
+                                name="type"
+                                required
+                            />
+                        </FormField>
+                        <FormField label="Power Level">
+                            <Input
+                                name="powerLevel"
+                                required
+                            />
+                        </FormField>
+                        <FormField label="Image">
+                            <Input
+                                name="Image"
+                            />
+                        </FormField>
+                    </Stack>
+                </Form>
+            </ToolModal>
+        </>
+    )
 }
 
 export default WelcomeToDragons
--- apps/expressApps/routes/dragonsController.ts
+++ apps/expressApps/routes/dragonsController.ts
@@ -2,38 +2,39 @@ import express, {NextFunction, Request, response, Response} from 'express';
 import bodyParser from 'body-parser';
 // @ts-ignore
 import {DragonType} from "dragons/src/dragonType";
-// import { DynamicsWebApi } from "dynamics-web-api";
-import { autoFormatDataKeys, getDynamicsWebApi
+import { DynamicsWebApi } from "dynamics-web-api";
+import {
+    authenticateUser, autoFormatDataKeys, autoReformatDataKeys, getDynamicsWebApi
 } from '@churchofjesuschrist/amulek-cumorah';
 import axios from "axios";
-
+import { v4 as uuidv4 } from 'uuid';
 const dynamicsWebApi = getDynamicsWebApi('misamulek-dev');
 
 const dragonsController = express();
 dragonsController.use(bodyParser.json());
 
-const collection =  'vincent_MonsterApp_dragons';
+const collection =  'new_dragons';
 
 dragonsController.get('/api/:dragonID',(req: Request, res: Response, next: NextFunction) => {
     let dragon; // const dragon: DragonType = res??
-    console.log(req.params);
+    //console.log(req.params);
     res.send(dragon)
 });
 
 //get all
 dragonsController.get('/all',async (req: Request, res: Response, next: NextFunction) => {
     try {
-        console.log("above await");
+       // console.log("above await");
         const response = await dynamicsWebApi.retrieve({
             collection: 'new_dragons'
         });
-        console.log("after await");
-        console.log(response);
+       // console.log("after await");
+       // console.log(response);
 
         const dragons = response.value;
-        console.log(dragons);
+       // console.log(dragons);
         const formattedDragons = autoFormatDataKeys(dragons, 'new');
-        console.log("formatted dragons", formattedDragons);
+       console.log("formatted dragons", formattedDragons);
         res.send(formattedDragons);
     } catch (error) {
         console.error('Error retrieving dragons:', error);
@@ -47,14 +48,29 @@ dragonsController.get('/all',async (req: Request, res: Response, next: NextFunct
 //
 // });
 
-dragonsController.post('/api/post',(req: Request, res: Response, next: NextFunction) => {
-    console.log(req.body);
-    return res.sendStatus(200);
 
-});
+
+dragonsController.post('/api/post',
+    async (req: any, res) => {
+        const dragonId = req.body?.dragonId || uuidv4();
+        const dragon: DragonType = {
+            ...req.body,
+            dragonId
+        };
+
+        console.log('in post', dragon);
+
+        await dynamicsWebApi.upsert({
+            key: `new_dragon_id='${dragonId}'`,
+            collection,
+            data: autoReformatDataKeys(dragon, 'new')
+        });
+
+        res.send({dragonId});
+    });
 
 dragonsController.delete('/api/delete/:dragon',(req: Request, res: Response, next: NextFunction) => {
-    console.log(req.params);
+    //console.log(req.params);
     return res.sendStatus(200);
 });
 
